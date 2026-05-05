03cb541128cf5272155e0d868c8e194f00226d58
felioune nour el houda <studentg390@gmail.com>
2026-05-05 22:30:08 +0100

    refactor(rename): improve variable and method names in Calculator

---
+++ 1_rename/CalculatorBefore.java
@@ -0,0 +1,15 @@
+
+public class CalculatorBefore {
+
+    
+    public double calc(double a, double b) {
+        double x = a + b;       
+        double y = a * b;       
+        return x / y;
+    }
+
+    
+    public void prtRes(double res) {
+        System.out.println("Result: " + res);
+    }
+}
